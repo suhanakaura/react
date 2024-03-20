be9c09f388531100203b22cf791619d5af937608
suhana <suhanakaura01@gmail.com>
2024-03-20 17:27:30 +0530

    js

---
+++ myapp/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// functional component - function returning html code
+function Navbar() {
+    return (
+      // html code(jsx code)-origin point
+      <div>
+        <h1>this is my navigation bar</h1>
+      </div>
+    );
+  }
+  
+  export default Navbar;
+  
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+root.render( //we can render only one tag. encapsulate them into one tag
+  // <React.StrictMode>
+  // fragment
+    <> 
+    <Navbar/>
     <App />
-  </React.StrictMode>
+    </>
+  // </React.StrictMode>
 );
